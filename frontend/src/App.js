a0bfdd93d8ad643b2820fef5b48ff49ce4b95a53
DiegopP <darktro900@gmail.com>
2024-08-08 20:39:53 -0500

    Header section,buscar, login, usuario y carro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-    <Outlet/>
+    <Header/>
+    <main>
+      <Outlet/>
+    </main>
+    <Footer/>
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Logo from './Logo'
+import { FaSearch } from "react-icons/fa";
+import { HiUserCircle } from "react-icons/hi";
+import { FaCartShopping } from "react-icons/fa6";
+
+
+
+const Header = () => {
+  return (
+    <header className='h-16 shadow-md' >
+      <div className='h-full container mx-auto flex items-center px-4 justify-between'>
+        <div className=''>
+            <Logo w ={70} h ={60}/>
+        </div>
+
+        <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
+            <input type='text' placeholder='Buscar' className='w-full outline-none pl-2'></input>
+            <div className='text-lg min-w-[50px] h-8 bg-green-600 flex items-center justify-center rounded-r-full text-white'>
+                <FaSearch/>
+            </div>
+        </div>
+
+        <div className='flex items-center gap-5'>
+            <div className='text-3xl cursor-pointer'>
+                <HiUserCircle />
+            </div>
+
+            <div className='text-2xl relative'>
+                <span><FaCartShopping /></span>
+                <div className='bg-green-700 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-3 -right-3'>
+                    <p className='text-sm'>0</p>
+                </div>   
+            </div>
+
+            <div>
+                <button className='px-3 py-1 rounded-full text-white bg-green-600 hover:hg-green-500'>Login</button>
+            </div>
+        </div>
+        
+    </div>
+    
+    </header>    
+  )
+}
+
+export default Header
